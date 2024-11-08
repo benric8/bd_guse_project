e60b150ed08f6b2d5536d711b814cd00fa58f39f
benric8 <43308390+benric8@users.noreply.github.com>
2024-11-08 03:54:28 -0500

    Update 09. INSERT_TIPO_DOCUMENTO.sql

--- 3. Guse/09. INSERT_TIPO_DOCUMENTO.sql
+++ 3. Guse/09. INSERT_TIPO_DOCUMENTO.sql
@@ -27,7 +27,7 @@ IF NOT EXISTS (SELECT 1
                WHERE UPPER(TRIM(x_tipo_documento)) = 'SIN DOCUMENTO'
                  AND UPPER(TRIM(x_abreviatura)) = '<NINGUNO>') THEN
     INSERT INTO serviciosexternos.mae_tipo_documento(b_tipo_documento, x_tipo_documento, x_abreviatura, l_activo, f_aud, b_aud, c_aud_uid, c_aud_uidred, c_aud_pc, c_aud_ip, c_aud_mcaddr) 
-    VALUES ('0', 'SIN DOCUMENTO', '<Ninguno>', '1', var_f_aud, var_b_aud, var_c_aud_uid, var_c_aud_uidred, var_c_aud_pc, var_c_aud_ip, var_c_aud_mcaddr);
+    VALUES ('0', 'SIN DOCUMENTO', '<Ninguno>', '0',  var_f_aud, var_b_aud, var_c_aud_uid, var_c_aud_uidred, var_c_aud_pc, var_c_aud_ip, var_c_aud_mcaddr);
     RAISE NOTICE 'Se inserto el tipo de documento SIN DOCUMENTO';
 ELSE
     RAISE NOTICE 'Ya estaba registrado el tipo de documento SIN DOCUMENTO';
@@ -63,7 +63,7 @@ IF NOT EXISTS (SELECT 1
                WHERE UPPER(TRIM(x_tipo_documento)) = 'PASAPORTE'
                  AND UPPER(TRIM(x_abreviatura)) = 'P.S') THEN
     INSERT INTO serviciosexternos.mae_tipo_documento(b_tipo_documento, x_tipo_documento, x_abreviatura, l_activo, f_aud, b_aud, c_aud_uid, c_aud_uidred, c_aud_pc, c_aud_ip, c_aud_mcaddr) 
-    VALUES ('5', 'PASAPORTE', 'P.S', '1', var_f_aud, var_b_aud, var_c_aud_uid, var_c_aud_uidred, var_c_aud_pc, var_c_aud_ip, var_c_aud_mcaddr);
+    VALUES ('5', 'PASAPORTE', 'P.S', '0',var_f_aud, var_b_aud, var_c_aud_uid, var_c_aud_uidred, var_c_aud_pc, var_c_aud_ip, var_c_aud_mcaddr);
     RAISE NOTICE 'Se inserto el tipo de documento PASAPORTE';
 ELSE
     RAISE NOTICE 'Ya estaba registrado el tipo de documento PASAPORTE';
@@ -75,7 +75,7 @@ IF NOT EXISTS (SELECT 1
                WHERE UPPER(TRIM(x_tipo_documento)) = 'CARNET DE EXTRANJERIA'
                  AND UPPER(TRIM(x_abreviatura)) = 'C.E') THEN
     INSERT INTO serviciosexternos.mae_tipo_documento(b_tipo_documento, x_tipo_documento, x_abreviatura, l_activo, f_aud, b_aud, c_aud_uid, c_aud_uidred, c_aud_pc, c_aud_ip, c_aud_mcaddr) 
-    VALUES ('6', 'CARNET DE EXTRANJERIA', 'C.E', '1', var_f_aud, var_b_aud, var_c_aud_uid, var_c_aud_uidred, var_c_aud_pc, var_c_aud_ip, var_c_aud_mcaddr);
+    VALUES ('6', 'CARNET DE EXTRANJERIA', 'C.E', '0' ,var_f_aud, var_b_aud, var_c_aud_uid, var_c_aud_uidred, var_c_aud_pc, var_c_aud_ip, var_c_aud_mcaddr);
     RAISE NOTICE 'Se inserto el tipo de documento CARNET DE EXTRANJERIA';
 ELSE
     RAISE NOTICE 'Ya estaba registrado el tipo de documento CARNET DE EXTRANJERIA';
@@ -87,7 +87,7 @@ IF NOT EXISTS (SELECT 1
                WHERE UPPER(TRIM(x_tipo_documento)) = 'CARNET DE TRABAJO'
                  AND UPPER(TRIM(x_abreviatura)) = 'C.T.') THEN
     INSERT INTO serviciosexternos.mae_tipo_documento(b_tipo_documento, x_tipo_documento, x_abreviatura, l_activo, f_aud, b_aud, c_aud_uid, c_aud_uidred, c_aud_pc, c_aud_ip, c_aud_mcaddr) 
-    VALUES ('H', 'CARNET DE TRABAJO', 'C.T.', '1', var_f_aud, var_b_aud, var_c_aud_uid, var_c_aud_uidred, var_c_aud_pc, var_c_aud_ip, var_c_aud_mcaddr);
+    VALUES ('H', 'CARNET DE TRABAJO', 'C.T.', '0', var_f_aud, var_b_aud, var_c_aud_uid, var_c_aud_uidred, var_c_aud_pc, var_c_aud_ip, var_c_aud_mcaddr);
     RAISE NOTICE 'Se insertó el tipo de documento CARNET DE TRABAJO';
 ELSE
     RAISE NOTICE 'Ya estaba registrado el tipo de documento CARNET DE TRABAJO';
@@ -95,5 +95,3 @@ END IF;
 
 COMMIT;
 END $$;
-
-
