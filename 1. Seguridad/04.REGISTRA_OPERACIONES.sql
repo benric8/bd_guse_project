0d28613a0ce4ed35dfc0de323ea13ab15fb54211
benric8 <benric.366@gmail.com>
2024-11-20 18:02:19 -0500

    script para perfiles modificados

--- 1. Seguridad/04.REGISTRA_OPERACIONES.sql
+++ 1. Seguridad/04.REGISTRA_OPERACIONES.sql
@@ -460,5 +460,44 @@ IF NOT EXISTS(SELECT 1 FROM seguridad.mae_operacion WHERE x_endpoint='/servicios
 	ELSE
 		RAISE NOTICE 'Ya estaba registrado la operacion con endpoint /servicios-externos-api/admin/perfiles';
 	END IF;
+
+	IF NOT EXISTS(SELECT 1 FROM seguridad.mae_operacion WHERE x_endpoint='/servicios-externos-api/personas/combo'  AND
+					n_rol=(SELECT n_rol FROM seguridad.mae_rol WHERE C_ROL='AGUSE02'))
+	THEN
+		INSERT INTO seguridad.mae_operacion
+			(n_operacion, n_aplicativo, n_rol, x_operacion, x_descripcion, x_endpoint, l_activo,
+			f_aud, b_aud, c_aud_uid, c_aud_uidred, c_aud_pc, c_aud_ip, c_aud_mcaddr)
+		VALUES ( Nextval('seguridad.mae_operacion_seq'),
+		(SELECT n_aplicativo FROM seguridad.mae_aplicativo WHERE c_aplicativo = 'GUSEA'),
+		(SELECT n_rol FROM seguridad.mae_rol WHERE c_rol ='AGUSE02'),
+		'/personas/combo',
+		'Método para listar las personas en combo.',
+		'/servicios-externos-api/personas/combo',
+		'1',
+		var_f_aud, var_b_aud, var_c_aud_uid, var_c_aud_uidred,var_c_aud_pc,var_c_aud_ip,var_c_aud_mcaddr);
+	 	RAISE NOTICE 'Se creo la operacion con endpoint /servicios-externos-api/personas/combo';
+	ELSE
+		RAISE NOTICE 'Ya estaba registrado la operacion con endpoint /servicios-externos-api/personas/combo';
+	END IF;	
+
+	
+IF NOT EXISTS(SELECT 1 FROM seguridad.mae_operacion WHERE x_endpoint='/servicios-externos-api/aplicativos/combo'  AND
+					n_rol=(SELECT n_rol FROM seguridad.mae_rol WHERE C_ROL='AGUSE02'))
+	THEN
+		INSERT INTO seguridad.mae_operacion
+			(n_operacion, n_aplicativo, n_rol, x_operacion, x_descripcion, x_endpoint, l_activo,
+			f_aud, b_aud, c_aud_uid, c_aud_uidred, c_aud_pc, c_aud_ip, c_aud_mcaddr)
+		VALUES ( Nextval('seguridad.mae_operacion_seq'),
+		(SELECT n_aplicativo FROM seguridad.mae_aplicativo WHERE c_aplicativo = 'GUSEA'),
+		(SELECT n_rol FROM seguridad.mae_rol WHERE c_rol ='AGUSE02'),
+		'/aplicativos/combo',
+		'Método para listar aplicativos en combos',
+		'/servicios-externos-api/aplicativos/combo',
+		'1',
+		var_f_aud, var_b_aud, var_c_aud_uid, var_c_aud_uidred,var_c_aud_pc,var_c_aud_ip,var_c_aud_mcaddr);
+	 	RAISE NOTICE 'Se creo la operacion con endpoint /servicios-externos-api/aplicativos/combo';
+	ELSE
+		RAISE NOTICE 'Ya estaba registrado la operacion con endpoint /servicios-externos-api/aplicativos/combo';
+	END IF;
 COMMIT;
 END $$;
